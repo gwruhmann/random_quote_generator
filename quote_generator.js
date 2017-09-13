7dd397fbbcbc318ead94bcb896e29a2d37758e34
Garrett Ruhmann <gwruhmann@gmail.com>
2017-09-13 18:17:17 -0400

    Add Files
    Create initial files

---
+++ quote_generator.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#button").click(function() {
+    $.ajax({
+      cache: false,
+      url:
+        "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+      success: function(result) {
+        $("#quote").html(result[0].content);
+        $("#author").html("--" + result[0].title);
+      }
+    });
+  });
+  $(".tweet").click(function() {
+    window.open(
+      "https://twitter.com/intent/tweet?text=" +
+        $("#quote").text() +
+        $("#author").text().replace(/\"/g, "")
+    );
+  });
+});
